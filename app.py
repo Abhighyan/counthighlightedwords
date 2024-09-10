186eada24c5fab89a1d2d71a562fafe091b14d38
Abhighyan <92105888+Abhighyan@users.noreply.github.com>
2024-09-10 22:03:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def upload_and_count():
 
 if __name__ == '__main__':
     # Use the PORT environment variable provided by Railway
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
